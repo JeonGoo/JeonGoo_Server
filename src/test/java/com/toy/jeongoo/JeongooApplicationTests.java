a3fa8768357f07f1c006148a24ea017b170bddda
BangKiHyun <rlrlvh@naver.com>
2021-03-10 15:54:12 +0900

    init

---
+++ src/test/java/com/toy/jeongoo/JeongooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.jeongoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeongooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
