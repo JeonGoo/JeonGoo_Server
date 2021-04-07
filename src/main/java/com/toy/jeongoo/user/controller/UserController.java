373fbf78a879dd8d664542345b8c98380da60a55
BangKiHyun <rlrlvh@naver.com>
2021-04-07 16:05:26 +0900

    feat: add user sign in logic

--- src/main/java/com/toy/jeongoo/user/controller/UserController.java
+++ src/main/java/com/toy/jeongoo/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.toy.jeongoo.user.controller;
 
+import com.toy.jeongoo.user.controller.dto.request.SignInRequest;
 import com.toy.jeongoo.user.controller.dto.request.SignUpRequest;
+import com.toy.jeongoo.user.controller.dto.response.SignInResponse;
 import com.toy.jeongoo.user.service.LoginService;
 import com.toy.jeongoo.utils.DefaultResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.toy.jeongoo.utils.ResponseMessage.*;
@@ -19,13 +22,24 @@ public class UserController {
     private final LoginService loginService;
 
     @PostMapping("/signup")
-    public DefaultResponse<Long> signUp(SignUpRequest signUpRequest) {
+    public DefaultResponse<Long> signUp(@RequestBody SignUpRequest signUpRequest) {
         try {
             final Long signUpNumber = loginService.signUp(signUpRequest);
             return DefaultResponse.res(CREATED, CREATE_USER, signUpNumber);
-        } catch (Exception signUpException) {
-            log.error(signUpException.getMessage());
+        } catch (Exception loginException) {
+            log.error(loginException.getMessage());
             return DefaultResponse.res(BAD_REQUEST, CREATE_USER_FAIL);
+        }
+    }
+
+    @PostMapping("/signin")
+    public DefaultResponse<SignInResponse> signIn(@RequestBody SignInRequest signUpRequest) {
+        try {
+            final SignInResponse signInResponse = loginService.signIn(signUpRequest.getEmail(), signUpRequest.getPassword());
+            return DefaultResponse.res(OK, LOGIN_USER, signInResponse);
+        } catch (Exception loginException) {
+            log.error(loginException.getMessage());
+            return DefaultResponse.res(BAD_REQUEST, LOGIN_USER_FAIL);
         }
     }
 }
