0adfd728261a3bac79d6c471e42064de13e96539
BangKiHyun <rlrlvh@naver.com>
2021-04-16 14:34:08 +0900

    feat: setting bean of jpa query factory

--- src/main/java/com/toy/jeongoo/JeongooApplication.java
+++ src/main/java/com/toy/jeongoo/JeongooApplication.java
@@ -1,13 +1,23 @@
 package com.toy.jeongoo;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JeongooApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JeongooApplication.class, args);
     }
 
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
