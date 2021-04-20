c646ad857b9be277df2ea9d23944c04502903fd2
BangKiHyun <rlrlvh@naver.com>
2021-04-20 14:29:15 +0900

    refactor(ProductRegistrationService.class): use UserFindService instead of UserRepository

--- src/main/java/com/toy/jeongoo/product/service/ProductRegistrationService.java
+++ src/main/java/com/toy/jeongoo/product/service/ProductRegistrationService.java
@@ -7,35 +7,29 @@ import com.toy.jeongoo.product.api.dto.request.ProductBasicInfoRequest;
 import com.toy.jeongoo.product.model.Product;
 import com.toy.jeongoo.product.repository.ProductRepository;
 import com.toy.jeongoo.user.model.User;
-import com.toy.jeongoo.user.repository.UserRepository;
+import com.toy.jeongoo.user.service.UserFindService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class ProductRegistrationService {
 
     private final ProductRepository productRepository;
-    private final UserRepository userRepository;
+    private final UserFindService userFindService;
     private final FileService fileService;
 
     @Transactional
     public Long register(ProductBasicInfoRequest productBasicInfo, FileInfoRequest fileInfo, Long userId) {
-        final User user = findUserById(userId);
+        final User user = userFindService.findUser(userId);
         final List<File> uploadedFileList = fileService.upload(fileInfo);
         final Product product = new Product(productBasicInfo.getName(), productBasicInfo.getPrice(), productBasicInfo.getSerialNumber(),
                 productBasicInfo.getDescription(), productBasicInfo.getUseStatus(), user, uploadedFileList);
         productRepository.save(product);
 
         return product.getId();
-    }
-
-    private User findUserById(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException(String.format("not found user. input id: %d", id)));
     }
 }
