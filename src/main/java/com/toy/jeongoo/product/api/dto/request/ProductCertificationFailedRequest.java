feb95badc20a5dbfb0e6485455e01cdec8e06a9a
BangKiHyun <rlrlvh@naver.com>
2021-04-21 16:14:00 +0900

    refactor: add validate to product certification

--- src/main/java/com/toy/jeongoo/product/api/dto/request/ProductCertificationFailedRequest.java
+++ src/main/java/com/toy/jeongoo/product/api/dto/request/ProductCertificationFailedRequest.java
@@ -1,13 +1,18 @@
 package com.toy.jeongoo.product.api.dto.request;
 
-import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @NoArgsConstructor
 public class ProductCertificationFailedRequest {
 
-    @NotNull
+    @NotEmpty(message = "상품 인증 실패 사유를 알려줘야 합니다.")
     private String certificationFailedReason;
+
+    public ProductCertificationFailedRequest(@NotEmpty String certificationFailedReason) {
+        this.certificationFailedReason = certificationFailedReason;
+    }
 }
