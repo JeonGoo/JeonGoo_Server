02c4b3c15a0e999bfb2dd95674164190d5c61b90
BangKiHyun <rlrlvh@naver.com>
2021-04-21 17:24:40 +0900

    test: add certifyFailed() test code for product domain
    상품 도메인의 certifyFailed() 테스트 코드 추가

--- src/test/java/com/toy/jeongoo/product/model/ProductTest.java
+++ src/test/java/com/toy/jeongoo/product/model/ProductTest.java
@@ -5,8 +5,6 @@ import com.toy.jeongoo.product.model.status.UseStatus;
 import com.toy.jeongoo.user.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 
@@ -19,11 +17,7 @@ class ProductTest {
     @DisplayName("상품 인증이 완료되면 인증 상태가 COMPLETED로 바뀐다.")
     public void certifyTest() throws Exception {
         //given
-        User user = User.builder()
-                .name("bang")
-                .build();
-        Product product = new Product("상품", 10000L, "12345",
-                "좋은 상품", UseStatus.USED, user, new ArrayList<>());
+        Product product = createProduct();
 
         //when
         product.certify(ProductGrade.HIGH);
@@ -32,16 +26,11 @@ class ProductTest {
         assertThat(product.getCertificationStatus()).isEqualTo(CertificationStatus.COMPLETED);
     }
 
-    @ParameterizedTest
-    @DisplayName("상품 인증시 이미 인증된 상품이거나 인증이 실패된 상품이면 오류가 발생한다.")
-    @CsvSource("COMPLETED, FAILED")
-    public void certifyCanCertificationException(CertificationStatus certificationStatus) throws Exception {
+    @Test
+    @DisplayName("상품 인증시 이미 인증된 상품이면 오류가 발생한다.")
+    public void certifyCanCertificationExceptionTest() throws Exception {
         //given
-        User user = User.builder()
-                .name("bang")
-                .build();
-        Product product = new Product("상품", 10000L, "12345",
-                "좋은 상품", UseStatus.USED, user, new ArrayList<>());
+        Product product = createProduct();
         product.certify(ProductGrade.HIGH);
 
         //then
@@ -52,17 +41,53 @@ class ProductTest {
 
     @Test
     @DisplayName("상품 인증시 상품 등급이 NONE이면 오류 발생")
-    public void certifyGradeIsNotNoneException() throws Exception {
+    public void certifyGradeIsNotNoneExceptionTest() throws Exception {
         //given
-        User user = User.builder()
-                .name("bang")
-                .build();
-        Product product = new Product("상품", 10000L, "12345",
-                "좋은 상품", UseStatus.USED, user, new ArrayList<>());
+        Product product = createProduct();
 
         //then
         assertThatThrownBy(() -> product.certify(ProductGrade.NONE))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("product grade should not be none!");
+    }
+
+    @Test
+    @DisplayName("상품 인증 상태를 FAILED로 바꾼다.")
+    public void certifyFailedTest() throws Exception {
+        //given
+        Product product = createProduct();
+        String certificationFailedReason = "정품 인증이 불가한 상품입니다.";
+
+        //when
+        product.certifyFailed(certificationFailedReason);
+
+        //then
+        assertThat(product.getCertificationStatus()).isEqualTo(CertificationStatus.FAILED);
+        assertThat(product.getCertificationFailedReason()).isEqualTo(certificationFailedReason);
+    }
+
+    @Test
+    @DisplayName("이미 인증이 실패한 상품일 경우 오류 발생")
+    public void certifyFailedExceptionTest() throws Exception {
+        //given
+        Product product = createProduct();
+        String certificationFailedReason = "정품 인증이 불가한 상품입니다.";
+
+        //when
+        product.certifyFailed(certificationFailedReason);
+
+        //then
+        assertThatThrownBy(() -> product.certifyFailed(certificationFailedReason))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("the product has already been failed to certification.");
+    }
+
+    private Product createProduct() {
+        User user = User.builder()
+                .name("bang")
+                .build();
+
+        return new Product("상품", 10000L, "12345",
+                "좋은 상품", UseStatus.USED, user, new ArrayList<>());
     }
 }
