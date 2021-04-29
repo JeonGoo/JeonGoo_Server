9ffbdb43dd852ef83cbc73bdb899ffb848246397
BangKiHyun <rlrlvh@naver.com>
2021-04-29 15:04:25 +0900

    feat: add product purchase logic

--- src/main/java/com/toy/jeongoo/product/model/purchased/PurchasedProduct.java
+++ src/main/java/com/toy/jeongoo/product/model/purchased/PurchasedProduct.java
@@ -1,4 +1,4 @@
-package com.toy.jeongoo.product.model.purchase;
+package com.toy.jeongoo.product.model.purchased;
 
 import com.toy.jeongoo.product.model.Product;
 import com.toy.jeongoo.user.model.User;
@@ -14,18 +14,30 @@ import static lombok.AccessLevel.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-public class PurchaseProduct {
+public class PurchasedProduct {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "purchase_product_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
-    private User user;
+    private User purchasedUser;
+
+    public PurchasedProduct(Product product, User purchasedUser) {
+        checkSalesStatus(product);
+        this.product = product;
+        this.purchasedUser = purchasedUser;
+    }
+
+    private void checkSalesStatus(Product product) {
+        if (product.isSoldOut()) {
+            throw new IllegalArgumentException("This product has already been sold.");
+        }
+    }
 }
---
+++ src/main/java/com/toy/jeongoo/product/repository/purchase/PurchasedProductRepository.java
@@ -0,0 +1,7 @@
+package com.toy.jeongoo.product.repository.purchase;
+
+import com.toy.jeongoo.product.model.purchased.PurchasedProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PurchasedProductRepository extends JpaRepository<PurchasedProduct, Long> {
+}
