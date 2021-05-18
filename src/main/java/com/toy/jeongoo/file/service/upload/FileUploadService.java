ec89e9b121201d41ffee3b901ccbe0843e979f8d
BangKiHyun <rlrlvh@naver.com>
2021-05-18 14:23:00 +0900

    refactor: delete FileUploadServiceImpl

--- src/main/java/com/toy/jeongoo/file/service/upload/FileUploadService.java
+++ src/main/java/com/toy/jeongoo/file/service/upload/FileUploadService.java
@@ -1,11 +1,18 @@
 package com.toy.jeongoo.file.service.upload;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @Service
-public interface FileUploadService {
-    String upload(MultipartFile file) throws IOException;
+@RequiredArgsConstructor
+public class FileUploadService {
+
+    private final FileUploader fileUploader;
+
+    public String upload(MultipartFile file) throws IOException {
+        return fileUploader.upload(file);
+    }
 }
