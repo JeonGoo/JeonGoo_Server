64a4f9ce0ac89951b7575fdfeb1fd0d5f1c9a1c7
BangKiHyun <rlrlvh@naver.com>
2021-05-18 14:16:06 +0900

    feat: add aws s3 properties

--- src/main/java/com/toy/jeongoo/config/PropertiesConfig.java
+++ src/main/java/com/toy/jeongoo/config/PropertiesConfig.java
@@ -1,10 +1,11 @@
 package com.toy.jeongoo.config;
 
 import com.toy.jeongoo.auth.jwt.properties.JwtProperties;
+import com.toy.jeongoo.file.properties.AwsS3Properties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, AwsS3Properties.class})
 public class PropertiesConfig {
 }
