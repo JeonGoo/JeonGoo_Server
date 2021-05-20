24e289166007e49044faf2d67defb2f58c3b8c2f
BangKiHyun <rlrlvh@naver.com>
2021-05-20 16:08:29 +0900

    docs: add ProductFind & Purchase api description

--- src/main/java/com/toy/jeongoo/product/api/ProductFindController.java
+++ src/main/java/com/toy/jeongoo/product/api/ProductFindController.java
@@ -7,6 +7,10 @@ import com.toy.jeongoo.product.model.interest.InterestProduct;
 import com.toy.jeongoo.product.service.ProductFindService;
 import com.toy.jeongoo.product.service.interest.InterestProductService;
 import com.toy.jeongoo.utils.DefaultResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -31,6 +35,11 @@ public class ProductFindController {
     private final ProductFindService productFindService;
     private final InterestProductService interestProductService;
 
+    @ApiOperation(value = "상품 조회", notes = "Product의 id로 상품을 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "상품 조회 성공"),
+            @ApiResponse(code = 400, message = "상품 조회 실패")
+    })
     @GetMapping("/{productId}")
     public DefaultResponse<ProductShowDetailResponse> showProductDetail(@PathVariable Long productId) {
         try {
@@ -42,6 +51,11 @@ public class ProductFindController {
         }
     }
 
+    @ApiOperation(value = "상품 및 관심 여부 조회", notes = "해당 상품에 대한 내용 및 관심 여부를 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "상품 조회 성공"),
+            @ApiResponse(code = 400, message = "상품 조회 실패")
+    })
     @GetMapping("/{productId}/users/{interestedUserId}")
     public DefaultResponse<ProductShowDetailWIthInterestedStatusResponse> showProductDetailWithInterestedStatus(@PathVariable Long productId,
                                                                                                                 @PathVariable Long interestedUserId) {
@@ -55,6 +69,11 @@ public class ProductFindController {
         }
     }
 
+    @ApiOperation(value = "상품 전체 조회", notes = "모든 상품을 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "상품 조회 성공"),
+            @ApiResponse(code = 400, message = "상품 조회 실패")
+    })
     @GetMapping
     public DefaultResponse<List<ProductShowDetailResponse>> showAllProductsDetailPage(Pageable pageable) {
         try {
@@ -68,9 +87,13 @@ public class ProductFindController {
         }
     }
 
+    @ApiOperation(value = "회원이 등록한 상품 조회", notes = "회원이 등록한 상품을 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "상품 조회 성공"),
+            @ApiResponse(code = 400, message = "상품 조회 실패")
+    })
     @GetMapping("/users/{userId}")
-    public DefaultResponse<List<ProductShowDetailResponse>> findAllProductPageByUser(@PathVariable Long userId,
-                                                                                     Pageable pageable) {
+    public DefaultResponse<List<ProductShowDetailResponse>> findAllProductPageByUser(@PathVariable Long userId, Pageable pageable) {
         try {
             final List<Product> productList = productFindService.findAllProductPageByUser(userId, pageable);
             return DefaultResponse.res(OK, SHOW_PRODUCT, toProductShowDetailResponseList(productList));
@@ -80,6 +103,11 @@ public class ProductFindController {
         }
     }
 
+    @ApiOperation(value = "판매중인 상품 조회", notes = "등록된 상품중 팔리지 않은 상품만 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "상품 조회 성공"),
+            @ApiResponse(code = 400, message = "상품 조회 실패")
+    })
     @GetMapping("/sale")
     public DefaultResponse<List<ProductShowDetailResponse>> showAllSaleProductsPage(Pageable pageable) {
         try {
