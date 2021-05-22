647a2284e0219ecfbe35c103463413b0850da918
BangKiHyun <rlrlvh@naver.com>
2021-05-22 16:56:48 +0900

    setting initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO user VALUES
+    (1, '중구', '신흥동 2가', 'test@test.com', 'MALE', 'bang', '1234', '1234-5678'),
+    (2, '동구', '삼익아파트', 'user@user.com', 'MALE', 'myung', '1234', '1234-1234'),
+    (3, '가람', '부평아파트', 'garam@garam.com', 'MALE', 'garam', '1234', '1234-1234');
+
+INSERT INTO product VALUES
+    (1, '', 'NONE', 'good product', 'NONE', 0, 'mac book', '500000', 'SALE', 'serial', 'USE', 1),
+    (2, '', 'REQUEST', 'so product', 'NONE', 0, 'iphone', '20000', 'SALE', 'serial', 'USE', 1),
+    (3, '', 'COMPLETED', 'really product', 'HIGH', 0, 'mac book pro', '1000000', 'SALE', 'serial', 'USE', 2),
+    (4, 'not found serial number', 'FAILED', 'wow product', 'NONE', 0, 'window', '200000', 'SALE', 'serial', 'USE', 2),
+    (5, '', 'NONE', 'feel good product', 'NONE', 0, 'galaxy', '20000', 'SALE', 'serial', 'USE', 3);
