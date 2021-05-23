0c8d1c1ed3ea0031e40c3c67c539781b6840446f
BangKiHyun <rlrlvh@naver.com>
2021-05-23 16:45:14 +0900

    fix: change initial data
    fix ProductCertificationStatus & UseStatus

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ INSERT INTO user VALUES
     (3, '가람', '부평아파트', 'garam@garam.com', 'MALE', 'garam', '1234', '1234-1234');
 
 INSERT INTO product VALUES
-    (1, '', 'NONE', 'good product', 'NONE', 0, 'mac book', '500000', 'SALE', 'serial', 'USE', 1),
-    (2, '', 'REQUEST', 'so product', 'NONE', 0, 'iphone', '20000', 'SALE', 'serial', 'USE', 1),
-    (3, '', 'COMPLETED', 'really product', 'HIGH', 0, 'mac book pro', '1000000', 'SALE', 'serial', 'USE', 2),
-    (4, 'not found serial number', 'FAILED', 'wow product', 'NONE', 0, 'window', '200000', 'SALE', 'serial', 'USE', 2),
-    (5, '', 'NONE', 'feel good product', 'NONE', 0, 'galaxy', '20000', 'SALE', 'serial', 'USE', 3);
+    (1, '', 'REQUEST', 'good product', 'NONE', 0, 'mac book', '500000', 'SALE', 'serial', 'USED', 1),
+    (2, '', 'REQUEST', 'so product', 'NONE', 0, 'iphone', '20000', 'SALE', 'serial', 'USED', 1),
+    (3, '', 'COMPLETED', 'really product', 'HIGH', 0, 'mac book pro', '1000000', 'SALE', 'serial', 'USED', 2),
+    (4, 'not found serial number', 'FAILED', 'wow product', 'NONE', 0, 'window', '200000', 'SALE', 'serial', 'USED', 2),
+    (5, '', 'REQUEST', 'feel good product', 'NONE', 0, 'galaxy', '20000', 'SALE', 'serial', 'USED', 3);
