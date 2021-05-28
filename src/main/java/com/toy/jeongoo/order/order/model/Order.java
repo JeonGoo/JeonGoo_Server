df32a1ca0de56b77a3515ad83095ef1a574fd105
BangKiHyun <rlrlvh@naver.com>
2021-05-28 21:34:37 +0900

    refactor: add BaseTimeEntity to Order & OrderLine

--- src/main/java/com/toy/jeongoo/order/order/model/Order.java
+++ src/main/java/com/toy/jeongoo/order/order/model/Order.java
@@ -1,5 +1,6 @@
 package com.toy.jeongoo.order.order.model;
 
+import com.toy.jeongoo.common.entity.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,7 @@ import static javax.persistence.GenerationType.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Order {
+public class Order extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
