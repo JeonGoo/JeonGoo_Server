6ad59e793e622bfd785dae54a5a29006ea32ef12
BangKiHyun <rlrlvh@naver.com>
2021-05-31 16:01:49 +0900

    refactor: fix error
    Order -> Orders
    add Order mapping in OrderLine

--- src/main/java/com/toy/jeongoo/order/product/model/OrderProduct.java
+++ src/main/java/com/toy/jeongoo/order/product/model/OrderProduct.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
@@ -13,16 +14,17 @@ import javax.persistence.Embeddable;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderProduct {
 
-    private Long productId;
+    @Column(name = "order_product_id")
+    private Long id;
 
     @Column(name = "order_product_name")
     private String name;
 
-    @Column(name = "order_product_price")
+    @AttributeOverride(name = "value", column = @Column(name = "order_product_price"))
     private Money price;
 
-    public OrderProduct(Long productId, String name, Money price) {
-        this.productId = productId;
+    public OrderProduct(Long id, String name, Money price) {
+        this.id = id;
         this.name = name;
         this.price = price;
     }
