215115721af176e408ae365d11f7d3119baaaaed
BangKiHyun <rlrlvh@naver.com>
2021-06-02 17:18:15 +0900

    feat: add kakao pay properties

--- src/main/java/com/toy/jeongoo/config/PropertiesConfig.java
+++ src/main/java/com/toy/jeongoo/config/PropertiesConfig.java
@@ -2,10 +2,11 @@ package com.toy.jeongoo.config;
 
 import com.toy.jeongoo.auth.jwt.properties.JwtProperties;
 import com.toy.jeongoo.file.properties.AwsS3Properties;
+import com.toy.jeongoo.payment.properties.PaymentProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({JwtProperties.class, AwsS3Properties.class})
+@EnableConfigurationProperties({JwtProperties.class, AwsS3Properties.class, PaymentProperties.class})
 public class PropertiesConfig {
 }
